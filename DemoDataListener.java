73c511f73099219ca79f11a7280f3443b5309b5a
CaoZhiJie <1119166456@qq.com>
2020-03-17 17:18:10 +0800

    提交

--- DemoDataListener.java
+++ DemoDataListener.java
@@ -47,7 +47,7 @@ public class DemoDataListener extends AnalysisEventListener<TestUser> {
      * @param demoDAO
      */
     public DemoDataListener(ITestUserService demoDAO) {
-		suos
+		suosz
         this.demoDAO = demoDAO;  
 		
     }
